d338a0cc02ddcf096801b2f9b7f6ce3993c0838d
andrewnx <architectonic@protonmail.com>
2023-11-27 20:19:35 -0330

    w

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,8 +17,9 @@ export default function Footer() {
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>  
-        </div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+        </div>    
       </div>
     </footer>
   )
