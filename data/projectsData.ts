6f6fb210014ea5a5ea08b0876dd150e0fd1962c5
andrewnx <architectonic@protonmail.com>
2023-11-28 18:57:58 -0330

    a

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,12 +8,11 @@ const projectsData = [
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Tuxedo Cat Coffee',
+    description: `Term Project for CP1212 Introductino to JavaScript.
+     `,
+    imgSrc: '/static/images/tuxedocat.webp',
+    href: 'https://tuxedocat.vercel.app/',
   },
 ]
 
