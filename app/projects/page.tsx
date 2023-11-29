d154eaaacb587354c051965523fde3db79e15d31
andrewnx <architectonic@protonmail.com>
2023-11-28 21:52:37 -0330

    a

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Listed chronologically from what I'm currently working on to what I first completed in college.
           </p>
         </div>
         <div className="container py-12">
