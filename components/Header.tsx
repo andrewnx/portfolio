0ffaec52269b94faca7d7f39996ffa21faaf32e9
andrewnx <architectonic@protonmail.com>
2023-12-06 10:56:09 -0330

    removed blog from nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
       </div>
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         {headerNavLinks
-          .filter((link) => link.href !== '/')
+          .filter((link) => link.href !== '/' && link.title !== 'Blog')
           .map((link) => (
             <Link
               key={link.title}
