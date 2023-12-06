9365a0b1a95b2ba9c2cb7639e6f283ee466bac66
andrewnx <architectonic@protonmail.com>
2023-12-06 14:08:58 -0330

    changed primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.pink,
+        primary: '#4ade80',
         gray: colors.gray,
       },
       typography: ({ theme }) => ({
