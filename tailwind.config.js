6aa5da9c416884eab2e657fb8dcb154d73ac5562
andrewnx <architectonic@protonmail.com>
2023-12-06 15:05:38 -0330

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: '#4ade80',
+        primary: colors.green,
         gray: colors.gray,
       },
       typography: ({ theme }) => ({
